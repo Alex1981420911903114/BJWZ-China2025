945c77dc1fb24867e593ad93fee4c2d7df783013
Alex198142 <123648927+Alex1981420911903114@users.noreply.github.com>
2025-08-27 16:05:22 +0800

    navbar

--- wiki/.vuepress/navbar.ts
+++ wiki/.vuepress/navbar.ts
@@ -4,27 +4,86 @@ export default navbar([
   "/",,
   "/demo/",
   {
-    text: "Guide",
-    icon: "lightbulb",
-    prefix: "/guide/",
+    text: "Team",
+    icon: "/assets/icon/people.svg",
+    prefix: "/team/",
     children: [
       {
-        text: "Bar",
-        icon: "lightbulb",
-        prefix: "bar/",
-        children: ["baz", { text: "...", icon: "ellipsis", link: "#" }],
+        text: "Attribute",
+        icon: "/assets/icon/star.svg",
+        prefix: "attribute/",
       },
       {
-        text: "Foo",
-        icon: "lightbulb",
-        prefix: "foo/",
-        children: ["ray", { text: "...", icon: "ellipsis", link: "#" }],
+        text: "The Team",
+        icon: "/assets/icon/people.svg",
+        prefix: "theteam/",
       },
     ],
   },
   {
-    text: "V2 Docs",
-    icon: "book",
-    link: "https://theme-hope.vuejs.press/",
+    text: "Project",
+    icon: "/assets/icon/flask.svg",
+    children: [
+      {
+        text: "Description",
+        icon: "/assets/icon/bookmarks.svg",
+        prefix: "description/",
+      },
+      {
+        text: "Design",
+        icon: "/assets/icon/pencil.svg",
+        prefix: "design/",
+      },
+      {
+        text: "Engineering",
+        icon: "/assets/icon/measuring-cup.svg",
+        prefix: "engineering/",
+      },
+      {
+        text: "Results",
+        icon: "/assets/icon/file-earmark-bar-graph.svg",
+        prefix: "results/",
+      },
+      {
+        text: "Model",
+        icon: "/assets/icon/calculator.svg",
+        prefix: "model/",
+      },
+      {
+        text: "Parts",
+        icon: "/assets/icon/lightning.svg",
+        prefix: "parts/",
+      },
+      {
+        text: "Hardware",
+        icon: "/assets/icon/gear.svg",
+        prefix: "hardware/",
+      },
+      {
+        text: "Safety",
+        icon: "/assets/icon/shield.svg",
+        prefix: "safety/",
+      },
+      {
+        text: "Contribution",
+        icon: "/assets/icon/person-arms-up.svg",
+        prefix: "contribution/",
+      },
+    ],
+  },
+  {
+    text: "Human Practices",
+    icon: "/assets/icon/person-walking.svg",
+    prefix: "humanpractices/",
+  },
+  {
+    text: "Notebook",
+    icon: "/assets/icon/book.svg",
+    prefix: "notebook/",
+  },
+  {
+    text: "Software",
+    icon: "/assets/icon/code-slash.svg",
+    prefix: "software/",
   },
 ]);
